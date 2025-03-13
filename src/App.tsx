cd17ca75e578e844ec2a4a6d769d804999910ecd
lucillo08 <lfpirox@gmail.com>
2025-03-13 10:58:28 +0000

    barbabraus aggiunta insieme id

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router";
 
+ export const Detail = () => {
+  const { id } = useParams();
+  return <div>barbabraus:{id}</div>
+ }
 export const App = () => {
   const [count, setCount] = useState(0);
   const [title, setTitle] = useState("barbara");
+
+  useEffect(() => {
+    if (count === 4) {
+      setTitle("4 barbarelle")
+    }
+  },[count])
 
   return (
     <div className="h-dvh flex flex-col items-center justify-center">
@@ -16,14 +27,14 @@ export const App = () => {
         <div className="flex flex-col items-center space-y-4">
           <button
             className="bg-blue-500 text-white px-4 py-2 rounded-md shadow-md cursor-pointer hover:bg-blue-600 transition-colors"
-            onClick={() => setCount((count) => count + 1)}
+            onClick={() => setCount((count) => count + 4)}
           >
             Hai premuto il pulsante {count} {count === 1 ? "volta" : "volte"}
           </button>
 
           <button
               className="bg-blue-500 text-white px-4 py-2 rounded-md shadow-md cursor-pointer hover:bg-blue-600 transition-colors"
-              onClick={() => setTitle("Charizard")}
+              onClick={() => setTitle("MEGA barbara")}
           >
             Cambia
           </button>
